529cded848d22d28386094174fac4600b7dbf963
su <sumayphoozin201912@gmail.com>
2023-11-13 10:45:21 +0900

    v0.3

--- week11/main.go
+++ week11/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var s []int
-	s = make([]int, 6)
+	s := []int{0, 0, 0, 0, 0} //slice literal
+	s[4] = 99
 	for _, value := range s {
 		fmt.Println(value)
 	}
