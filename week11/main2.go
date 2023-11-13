742520c0c7926150e1d590e2e04c6a5884101c81
su <sumayphoozin201912@gmail.com>
2023-11-13 11:21:07 +0900

    week12 v0.4

---
+++ week11/main2.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{0, 0, 0, 0, 0}
+
+	s[4] = 99
+	s[2] = 11
+	for i := 0; i < len(s); i++ {
+		fmt.Println(s[i])
+	}
+	copyS := s[1:4]
+	for _, value := range copyS {
+		fmt.Println(value)
+	}
+	test := [3]string{"inha", "go", "student"}
+	testS := test[0:2]
+	//testS := test[0:4] //error invalid argument index 4 out of bounds [0:4]
+	fmt.Println((len(testS)))
+}
