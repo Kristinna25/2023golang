3adca09d744cc9590cc660e8602bde102849f0e7
su <sumayphoozin201912@gmail.com>
2023-11-13 11:22:39 +0900

    wee12 v0.4

--- week11/main2.go
+++ week11/main2.go
@@ -15,7 +15,7 @@ func main() {
 		fmt.Println(value)
 	}
 	test := [3]string{"inha", "go", "student"}
-	testS := test[0:2]
+	testS := test[0:2] //testS := test[:2]
 	//testS := test[0:4] //error invalid argument index 4 out of bounds [0:4]
 	fmt.Println((len(testS)))
 }
