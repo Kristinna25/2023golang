3797ab028b169b45fae1f9fdff94c656062b32b8
su <sumayphoozin201912@gmail.com>
2023-11-13 11:38:07 +0900

    wee12 v3.4

---
+++ week11/main3.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := []string{"a", "b", "c", "d"}
+	as := a[0:2] //a,b
+	as[1] = "z"
+	fmt.Println(as)
+
+	b := [4]int{4, 3, 2, 1}
+	bs := b[1:3]
+	fmt.Println(bs)
+
+}
