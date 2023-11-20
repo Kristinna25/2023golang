a7e33cbb6a2143ee9a73310f0fe35d34665cf14b
su <sumayphoozin201912@gmail.com>
2023-11-20 10:47:25 +0900

    week13 v3.9

---
+++ week13th/3.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a []string
+	var b []bool
+
+	b = append(b, true)
+	fmt.Printf("%#v %#v\n", a, b)
+
+	fmt.Println(a, len(a), cap(a))
+	fmt.Println(b, len(b), cap(b))
+}
